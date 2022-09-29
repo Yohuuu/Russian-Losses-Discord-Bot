3d08b61fde8a4b3a66e32e5fdac0d8d810d7e826
Yohuuu <2008artemartemovich@gmail.com>
2022-09-29 19:51:30 +0300

    Добавьте файлы проекта.

---
+++ RussiaLossesBot/Modules/Commands.cs
@@ -0,0 +1,77 @@
+﻿using Discord;
+using Discord.Commands;
+using Newtonsoft.Json;
+
+
+namespace RussiaLossesBot.Modules
+{
+    public class Commands : ModuleBase
+    {
+        private HttpClient client = new HttpClient();
+
+        [Command("losses")]
+        public async Task TotalLosses()
+        {
+            try
+            {
+                HttpResponseMessage response = await client.GetAsync("https://russianwarship.rip/api/v1/statistics/latest");
+                response.EnsureSuccessStatusCode();
+                string responseBody = await response.Content.ReadAsStringAsync();
+                Increase losses = JsonConvert.DeserializeObject<Increase>(responseBody);
+                Data data = JsonConvert.DeserializeObject<Data>(responseBody);
+                Root root = JsonConvert.DeserializeObject<Root>(responseBody);
+
+                foreach(var item in Convert.ToString())
+                {
+                    int i = 0;
+                    i++;
+                    await ReplyAsync(Convert.ToString(i));
+                }
+
+                var embed = new EmbedBuilder()
+                    .WithDescription("")
+                    .WithTitle($"Total russian army losses for {root.data.date}")
+                    .WithAuthor(Context.Client.CurrentUser)
+                    .WithCurrentTimestamp()
+                    .WithColor(Discord.Color.Orange);
+                await ReplyAsync(embed: embed.Build());
+            }
+            catch (HttpRequestException e)
+            {
+                Console.WriteLine("\nException Caught!");
+                Console.WriteLine("Message :{0} ", e.Message);
+
+            }         
+        }
+        public class Increase
+        {
+            public int personnel_units { get; set; }
+            public int tanks { get; set; }
+            public int armoured_fighting_vehicles { get; set; }
+            public int artillery_systems { get; set; }
+            public int mlrs { get; set; }
+            public int aa_warfare_systems { get; set; }
+            public int planes { get; set; }
+            public int helicopters { get; set; }
+            public int vehicles_fuel_tanks { get; set; }
+            public int warships_cutters { get; set; }
+            public int cruise_missiles { get; set; }
+            public int uav_systems { get; set; }
+            public int special_military_equip { get; set; }
+            public int atgm_srbm_systems { get; set; }
+        }
+        public class Data
+        {
+            public string date { get; set; }
+            public int day { get; set; }
+            public string resource { get; set; }
+            public Increase increase { get; set; }
+        }
+        public class Root
+        {
+            public string message { get; set; }
+            public Data data { get; set; }
+        }
+
+    }
+}
---
+++ RussiaLossesBot/Program.cs
@@ -0,0 +1,69 @@
+﻿using Discord;
+using Discord.Commands;
+using Discord.Net;
+using Discord.WebSocket;
+using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json;
+using System;
+using System.Reflection;
+
+namespace RussiaLossesBot
+{
+    class Program
+    {
+        static void Main(string[] args) => new Program().RunBotAsync().GetAwaiter().GetResult();
+        private DiscordSocketClient _client;
+        private CommandService _commands;
+        private IServiceProvider _services;
+
+        public async Task RunBotAsync()
+        {
+            _client = new DiscordSocketClient();
+            _commands = new CommandService();
+            _services = new ServiceCollection()
+                .AddSingleton(_client)
+                .AddSingleton(_commands)
+                .BuildServiceProvider();
+
+            string token = "MTAyNDMyMjc3ODY3NTAzNjIwMA.GDVu4E.kQ2FZECi6LgOEXgCLF_vE4eKERJkUhKNjht7bw";
+
+            _client.Log += client_Log;
+
+            await RegisterCommandsAsync();
+
+            await _client.LoginAsync(TokenType.Bot, token);
+
+            await _client.StartAsync();
+
+        await Task.Delay(-1);
+        }
+
+        private Task client_Log(LogMessage arg)
+        {
+            Console.WriteLine(arg);
+            return Task.CompletedTask;
+        }
+
+        public async Task RegisterCommandsAsync()
+        {
+            _client.MessageReceived += HandleCommandAsync;
+            await _commands.AddModulesAsync(Assembly.GetEntryAssembly(), _services);
+        }
+
+        private async Task HandleCommandAsync(SocketMessage arg)
+        {
+            var message = arg as SocketUserMessage;
+            var context = new SocketCommandContext(_client, message);
+            if (message.Author.IsBot) return;
+
+            int argPos = 0;
+            if (message.HasStringPrefix("!", ref argPos))
+            {
+                var result = await _commands.ExecuteAsync(context, argPos, _services);
+                if (!result.IsSuccess) Console.WriteLine(result.ErrorReason);
+                if (result.Error.Equals(CommandError.UnmetPrecondition)) await message.Channel.SendMessageAsync(result.ErrorReason);
+
+            }
+        }
+    }
+}
